111b163b4ee1bb6c4b48ddc4c4729b067b871de2
liuhongwei <1120125168@qq.com>
2022-11-29 16:45:40 +0800

    dev server and client

---
+++ main/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"fmt"
+	"jzRpc"
+	"log"
+	"net"
+	"sync"
+	"time"
+)
+
+func startServer(addr chan string) {
+	// pick a free port
+	l, err := net.Listen("tcp", ":0")
+	if err != nil {
+		log.Fatal("network error:", err)
+	}
+	log.Println("start rpc server on", l.Addr())
+	// 保证监听端口成功
+	addr <- l.Addr().String()
+	// 开始rp服务
+	jzRpc.Accept(l)
+}
+
+// day 1 version
+/*
+func main() {
+	addr := make(chan string)
+	go startServer(addr)
+
+	// in fact, following code is like a simple geerpc client
+	conn, _ := net.Dial("tcp", <-addr)
+	defer func() { _ = conn.Close() }()
+
+	time.Sleep(time.Second)
+	// send options
+	_ = json.NewEncoder(conn).Encode(geerpc.DefaultOption)
+	cc := codec.NewGobCodec(conn)
+	// send request & receive response
+	for i := 0; i < 5; i++ {
+		h := &codec.Header{
+			ServiceMethod: "Foo.Sum",
+			Seq:           uint64(i),
+		}
+		_ = cc.Write(h, fmt.Sprintf("geerpc req %d", h.Seq))
+		_ = cc.ReadHeader(h)
+		var reply string
+		_ = cc.ReadBody(&reply)
+		log.Println("reply:", reply)
+	}
+}
+
+*/
+
+// day 2 version
+func main() {
+	log.SetFlags(0)
+	addr := make(chan string)
+	go startServer(addr)
+	client, _ := jzRpc.Dial("tcp", <-addr)
+	defer func() { _ = client.Close() }()
+
+	time.Sleep(time.Second)
+	// send request & receive response
+	var wg sync.WaitGroup
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			args := fmt.Sprintf("geerpc req %d", i)
+			var reply string
+			if err := client.Call("Foo.Sum", args, &reply); err != nil {
+				log.Fatal("call Foo.Sum error:", err)
+			}
+			log.Println("reply:", reply)
+		}(i)
+	}
+	wg.Wait()
+}
