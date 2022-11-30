7cb5248a7f219560dbe6a51ed09cd6eb8c21a6da
liuhongwei <1120125168@qq.com>
2022-11-30 22:12:34 +0800

    finally

--- main/main.go
+++ main/main.go
@@ -1,14 +1,5 @@
 package main
 
-import (
-	"context"
-	"jzRpc"
-	"log"
-	"net"
-	"sync"
-	"time"
-)
-
 //func startServer(addr chan string) {
 //	// pick a free port
 //	l, err := net.Listen("tcp", ":0")
@@ -79,7 +70,7 @@ func main() {
 //}
 
 //day 3 main
-
+/*
 type Foo int
 
 type Args struct{ Num1, Num2 int }
@@ -88,22 +79,30 @@ func (f Foo) Sum(args Args, reply *int) error {
 	*reply = args.Num1 + args.Num2
 	return nil
 }
-
-func startServer(addr chan string) {
+func startServer(addrCh chan string) {
 	var foo Foo
-	if err := jzRpc.Register(&foo); err != nil {
-		log.Fatal("register error:", err)
-	}
-	// pick a free port
-	l, err := net.Listen("tcp", ":0")
-	if err != nil {
-		log.Fatal("network error:", err)
-	}
-	log.Println("start rpc server on", l.Addr())
-	addr <- l.Addr().String()
-	jzRpc.Accept(l)
+	l, _ := net.Listen("tcp", ":9999")
+	_ = jzRpc.Register(&foo)
+	jzRpc.HandleHTTP()
+	addrCh <- l.Addr().String()
+	_ = http.Serve(l, nil)
 }
-
+*/
+//func startServer(addr chan string) {
+//	var foo Foo
+//	if err := jzRpc.Register(&foo); err != nil {
+//		log.Fatal("register error:", err)
+//	}
+//	// pick a free port
+//	l, err := net.Listen("tcp", ":0")
+//	if err != nil {
+//		log.Fatal("network error:", err)
+//	}
+//	log.Println("start rpc server on", l.Addr())
+//	addr <- l.Addr().String()
+//	jzRpc.Accept(l)
+//}
+/*
 func main() {
 	log.SetFlags(0)
 	addr := make(chan string)
@@ -127,4 +126,135 @@ func main() {
 		}(i)
 	}
 	wg.Wait()
+}
+*/
+/*
+func main() {
+	log.SetFlags(0)
+	ch := make(chan string)
+	go call(ch)
+	startServer(ch)
+}
+
+func call(addrCh chan string) {
+	client, _ := jzRpc.DialHTTP("tcp", <-addrCh)
+	defer func() { _ = client.Close() }()
+
+	time.Sleep(time.Second)
+	// send request & receive response
+	var wg sync.WaitGroup
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			args := &Args{Num1: i, Num2: i * i}
+			var reply int
+			if err := client.Call(context.Background(), "Foo.Sum", args, &reply); err != nil {
+				log.Fatal("call Foo.Sum error:", err)
+			}
+			log.Printf("%d + %d = %d", args.Num1, args.Num2, reply)
+		}(i)
+	}
+	wg.Wait()
+}
+*/
+
+import (
+	"context"
+	"jzRpc"
+	"jzRpc/xclient"
+	"log"
+	"net"
+	"sync"
+	"time"
+)
+
+type Foo int
+
+type Args struct{ Num1, Num2 int }
+
+func (f Foo) Sum(args Args, reply *int) error {
+	*reply = args.Num1 + args.Num2
+	return nil
+}
+
+func (f Foo) Sleep(args Args, reply *int) error {
+	time.Sleep(time.Second * time.Duration(args.Num1))
+	*reply = args.Num1 + args.Num2
+	return nil
+}
+
+func startServer(addrCh chan string) {
+	var foo Foo
+	l, _ := net.Listen("tcp", ":0")
+	server := jzRpc.NewServer()
+	_ = server.Register(&foo)
+	addrCh <- l.Addr().String()
+	server.Accept(l)
+}
+
+func foo(xc *xclient.XClient, ctx context.Context, typ, serviceMethod string, args *Args) {
+	var reply int
+	var err error
+	switch typ {
+	case "call":
+		err = xc.Call(ctx, serviceMethod, args, &reply)
+	case "broadcast":
+		err = xc.Broadcast(ctx, serviceMethod, args, &reply)
+	}
+	if err != nil {
+		log.Printf("%s %s error: %v", typ, serviceMethod, err)
+	} else {
+		log.Printf("%s %s success: %d + %d = %d", typ, serviceMethod, args.Num1, args.Num2, reply)
+	}
+}
+
+func call(addr1, addr2 string) {
+	d := xclient.NewMultiServerDiscovery([]string{"tcp@" + addr1, "tcp@" + addr2})
+	xc := xclient.NewXClient(d, xclient.RandomSelect, nil)
+	defer func() { _ = xc.Close() }()
+	// send request & receive response
+	var wg sync.WaitGroup
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			foo(xc, context.Background(), "call", "Foo.Sum", &Args{Num1: i, Num2: i * i})
+		}(i)
+	}
+	wg.Wait()
+}
+
+func broadcast(addr1, addr2 string) {
+	d := xclient.NewMultiServerDiscovery([]string{"tcp@" + addr1, "tcp@" + addr2})
+	xc := xclient.NewXClient(d, xclient.RandomSelect, nil)
+	defer func() { _ = xc.Close() }()
+	var wg sync.WaitGroup
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			foo(xc, context.Background(), "broadcast", "Foo.Sum", &Args{Num1: i, Num2: i * i})
+			// expect 2 - 5 timeout
+			ctx, _ := context.WithTimeout(context.Background(), time.Second*2)
+			foo(xc, ctx, "broadcast", "Foo.Sleep", &Args{Num1: i, Num2: i * i})
+		}(i)
+	}
+	wg.Wait()
+}
+
+func main() {
+	log.SetFlags(0)
+	ch1 := make(chan string)
+	ch2 := make(chan string)
+	// start two servers
+	go startServer(ch1)
+	go startServer(ch2)
+
+	addr1 := <-ch1
+	addr2 := <-ch2
+
+	time.Sleep(time.Second)
+	call(addr1, addr2)
+	broadcast(addr1, addr2)
 }
